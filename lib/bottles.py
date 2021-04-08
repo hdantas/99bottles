6cc53b94def06dacb3f2d0bbb80e0172579cd2a6
hdantas <hndantas@gmail.com>
2021-04-08 23:13:53 +0200

    Add solution for exercise 1

--- lib/bottles.py
+++ lib/bottles.py
@@ -1,50 +1,38 @@
 class Bottles:
-    MAX_BOTTLES = 99
-    MIN_BOTTLES = 0
+    @staticmethod
+    def pluralize_bottle(number) -> str:
+        return "bottles" if number > 1 else "bottle"
 
     @staticmethod
-    def capitalise(s: str) -> str:
-        result = ""
-        for i in s.splitlines():
-            result += i[0].upper() + i[1:] + "\n"
-        return result
-
-    @staticmethod
-    def bottle_of_beer(i: int) -> str:
-        i = i if i >= 0 else 99
-        bottle_s = "bottles" if i != 1 else "bottle"
-        number_s = i if i > 0 else "no more"
-        return f"{number_s} {bottle_s} of beer"
-
-    @staticmethod
-    def take_one_down(i: int) -> str:
+    def first_sentence(i) -> str:
+        bottle = Bottles.pluralize_bottle(i)
         if i > 0:
-            one_or_it = "one" if i else "it"
-            return f"Take {one_or_it} down and pass it around"
+            return f"{i} {bottle} of beer on the wall, {i} {bottle} of beer."
         else:
-            return "Go to the store and buy some more"
+            return "No more bottles of beer on the wall, no more bottles of beer."
 
     @staticmethod
-    def first_sentence(i: int) -> str:
-        bottle_of_beer = Bottles.bottle_of_beer(i)
-        return f"{bottle_of_beer} on the wall, {bottle_of_beer}."
-
-    @staticmethod
-    def second_sentence(i: int) -> str:
-        take_one_down = Bottles.take_one_down(i)
-        bottle_of_beer = Bottles.bottle_of_beer(i - 1)
-        return f"{take_one_down}, {bottle_of_beer} on the wall."
+    def second_sentence(i) -> str:
+        bottle = Bottles.pluralize_bottle(i-1)
+        if i > 1:
+            return f"Take one down and pass it around, {i-1} {bottle} of beer on the wall."
+        elif i == 1:
+            return "Take it down and pass it around, no more bottles of beer on the wall."
+        else:
+            return "Go to the store and buy some more, 99 bottles of beer on the wall."
 
     @staticmethod
     def verse(i: int) -> str:
-        sentences = f"{Bottles.first_sentence(i)}\n{Bottles.second_sentence(i)}\n"
-        return Bottles.capitalise(sentences)
+        return Bottles.first_sentence(i) + "\n" + Bottles.second_sentence(i) + "\n"
 
     @staticmethod
     def verses(start: int, end: int) -> str:
-        all_verses = [Bottles.verse(i) for i in range(start, end - 1, -1)]
-        return "\n".join(all_verses)
+        response = ""
+        for i in range(start, end - 1, -1):
+            response += Bottles.verse(i) + "\n"
+
+        return response[:-1]
 
     @staticmethod
-    def song() -> str:
-        return Bottles.verses(Bottles.MAX_BOTTLES, Bottles.MIN_BOTTLES)
+    def song():
+        return Bottles.verses(99, 0)
